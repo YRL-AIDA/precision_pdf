e727667fbe3f2d7713255f5d31124a6e19121e24
sinkudo <mishazagorodnikov@mail.ru>
2025-11-18 22:48:48 +0800

    Update PdfBoundingBoxRender

--- src/main/java/ru/sunveil/precision_pdf/pdfparser/visualizer/PdfBoundingBoxRenderer.java
+++ src/main/java/ru/sunveil/precision_pdf/pdfparser/visualizer/PdfBoundingBoxRenderer.java
@@ -15,59 +15,33 @@ import ru.sunveil.precision_pdf.pdfparser.model.Word;
 import ru.sunveil.precision_pdf.pdfparser.model.core.BoundingBox;
 
 public class PdfBoundingBoxRenderer {
-
+    public enum BoxType {
+        WORDS, LINES, CHUNKS
+    }
     /**
      * Нарисовать bounding box слов/линий/чанков
      *
      * @param inputPdf  исходный PDF-файл
      * @param outputPdf путь для сохранения PDF с рамками
      * @param pdfPages  результат парсинга PDF (список PdfPage)
+     * @param boxType какие bbox отрисовать на странице
      */
-    public void renderBoundingBoxes(File inputPdf, File outputPdf, List<PdfPage> pdfPages) throws IOException {
+    public void renderBoundingBoxes(File inputPdf, File outputPdf, List<PdfPage> pdfPages, BoxType boxType) throws IOException {
         try (PDDocument document = Loader.loadPDF(inputPdf)) {
             for (PdfPage pdfPage : pdfPages) {
                 int pageIndex = pdfPage.getPageNumber() - 1;
                 if (pageIndex < 0 || pageIndex >= document.getNumberOfPages()) continue;
 
                 PDPage page = document.getPage(pageIndex);
-                PDRectangle mediaBox = page.getMediaBox();
 
                 try (PDPageContentStream contentStream = new PDPageContentStream(document, page,
                         PDPageContentStream.AppendMode.APPEND, true, true)) {
 
-                    // слова
-//                    contentStream.setStrokingColor(Color.RED);
-//                    contentStream.setLineWidth(0.5f);
-//
-//                    for (Word word : pdfPage.getWords()) {
-//                        BoundingBox bbox = word.getBoundingBox();
-//                        float x = bbox.getX();
-//                        float y = bbox.getY();
-//                        float width = bbox.getWidth();
-//                        float height = bbox.getHeight();
-//                        contentStream.addRect(x, y, width, height);
-//                        contentStream.stroke();
-//                    }
-
-//                    // линии
-//                    contentStream.setStrokingColor(Color.BLUE);
-//                    contentStream.setLineWidth(0.3f);
-//                    for (TextLine line : pdfPage.getTextLines()) {
-//                        BoundingBox bbox = line.getBoundingBox();
-//                        float x = bbox.getX();
-//                        float y = bbox.getY();
-//                        float width = bbox.getWidth();
-//                        float height = bbox.getHeight();
-//                        contentStream.addRect(x, y, width, height);
-//                        contentStream.stroke();
-//                    }
-                    contentStream.setStrokingColor(Color.GREEN); // выбрал зелёный для отличия
-                    contentStream.setLineWidth(0.3f);
-
-                    // чанки
-                    for (PdfTextChunk chunk : pdfPage.getPdfTextChunks()) {
-                        BoundingBox bbox = chunk.getBoundingBox();
-                        if (bbox != null && bbox.isValid()) {
+                    if (boxType == BoxType.WORDS) {
+                        contentStream.setStrokingColor(Color.RED);
+                        contentStream.setLineWidth(0.5f);
+                        for (Word word : pdfPage.getWords()) {
+                            BoundingBox bbox = word.getBoundingBox();
                             float x = bbox.getX();
                             float y = bbox.getY();
                             float width = bbox.getWidth();
@@ -76,9 +50,38 @@ public class PdfBoundingBoxRenderer {
                             contentStream.stroke();
                         }
                     }
+
+                    if (boxType == BoxType.LINES) {
+                        contentStream.setStrokingColor(Color.BLUE);
+                        contentStream.setLineWidth(0.3f);
+                        for (TextLine line : pdfPage.getTextLines()) {
+                            BoundingBox bbox = line.getBoundingBox();
+                            float x = bbox.getX();
+                            float y = bbox.getY();
+                            float width = bbox.getWidth();
+                            float height = bbox.getHeight();
+                            contentStream.addRect(x, y, width, height);
+                            contentStream.stroke();
+                        }
+                    }
+
+                    if (boxType == BoxType.CHUNKS) {
+                        contentStream.setStrokingColor(Color.GREEN);
+                        contentStream.setLineWidth(0.3f);
+                        for (PdfTextChunk chunk : pdfPage.getPdfTextChunks()) {
+                            BoundingBox bbox = chunk.getBoundingBox();
+                            if (bbox != null && bbox.isValid()) {
+                                float x = bbox.getX();
+                                float y = bbox.getY();
+                                float width = bbox.getWidth();
+                                float height = bbox.getHeight();
+                                contentStream.addRect(x, y, width, height);
+                                contentStream.stroke();
+                            }
+                        }
+                    }
                 }
             }
-
             document.save(outputPdf);
         }
     }
