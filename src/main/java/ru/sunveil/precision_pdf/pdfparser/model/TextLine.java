028cb1887a13b0a4e22dd24ca3b951fb23cc24b5
sinkudo <mishazagorodnikov@mail.ru>
2025-11-18 22:54:35 +0800

    Added new fields for TextLine, Words and updated constructors

--- src/main/java/ru/sunveil/precision_pdf/pdfparser/model/TextLine.java
+++ src/main/java/ru/sunveil/precision_pdf/pdfparser/model/TextLine.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import ru.sunveil.precision_pdf.pdfparser.model.core.*;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,18 +13,30 @@ import java.util.List;
 public class TextLine extends TextEntity {
     private List<Word> words;
     private float lineHeight;
+    private int order;
+    private PDFFont font;
+    private Color color;
+    private float spaceWidth;
 
     public TextLine(){
         super();
         words = null;
         lineHeight = Float.MIN_VALUE;
+        order = -1;
+        font = null;
+        color = null;
+        spaceWidth = 0f;
     }
 
     public TextLine(int pageNumber, BoundingBox boundingBox, String text,
-                    List<Word> words, float lineHeight) {
+                    List<Word> words, float lineHeight, int order, PDFFont font, Color color, float spaceWidth) {
         super(pageNumber, boundingBox, text);
         this.words = words != null ? new ArrayList<>(words) : new ArrayList<>();
         this.lineHeight = lineHeight;
+        this.order = order;
+        this.font = font;
+        this.color = color;
+        this.spaceWidth = spaceWidth;
         if (lineHeight <= 0) {
             throw new IllegalArgumentException("Line height must be positive");
         }    }
--- src/main/java/ru/sunveil/precision_pdf/pdfparser/model/Word.java
+++ src/main/java/ru/sunveil/precision_pdf/pdfparser/model/Word.java
@@ -4,26 +4,41 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import ru.sunveil.precision_pdf.pdfparser.model.core.*;
 
+import java.awt.*;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Word extends TextEntity {
     private float confidence;
     private String fontName;
     private float fontSize;
+    private int order;
+    private PDFFont font;
+    private Color color;
+    private float spaceWidth;
 
     public Word(){
         super();
         confidence = Float.MIN_VALUE;
         fontName = null;
         fontSize = Float.MIN_VALUE;
+        order = -1;
+        font = null;
+        color = null;
+        spaceWidth = 0f;
     }
 
     public Word(int pageNumber, BoundingBox boundingBox, String text,
-                float confidence, String fontName, float fontSize) {
+                float confidence, String fontName, float fontSize, int order, PDFFont font, Color color, float spaceWidth) {
         super(pageNumber, boundingBox, text);
         this.confidence = confidence;
         this.fontName = fontName;
         this.fontSize = fontSize;
+        this.order = order;
+        this.font = font;
+        this.color = color;
+        this.spaceWidth = spaceWidth;
+
 
         if (confidence < 0 || confidence > 1) {
             throw new IllegalArgumentException("Confidence must be between 0 and 1");
