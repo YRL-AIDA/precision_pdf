ba159a867322e556d65483bf43009c4e0060ab79
sinkudo <mishazagorodnikov@mail.ru>
2026-01-14 15:38:29 +0800

    Update PdfPage.java

--- src/main/java/ru/sunveil/precision_pdf/pdfparser/model/PdfPage.java
+++ src/main/java/ru/sunveil/precision_pdf/pdfparser/model/PdfPage.java
@@ -1,11 +1,14 @@
 package ru.sunveil.precision_pdf.pdfparser.model;
 
 import lombok.Data;
+import ru.sunveil.precision_pdf.pdfparser.model.core.BoundingBox;
+import ru.sunveil.precision_pdf.pdfparser.model.core.PdfEntity;
+
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
-public class PdfPage {
+public class PdfPage implements PdfEntity {
     private int pageNumber;
     private double width;
     private double height;
@@ -14,4 +17,19 @@ public class PdfPage {
     private List<PdfTextChunk> pdfTextChunks = new ArrayList<>();
     private List<Table> tables = new ArrayList<>();
     private List<PdfImage> images = new ArrayList<>();
+
+    @Override
+    public BoundingBox getBoundingBox() {
+        return null;
+    }
+
+    @Override
+    public String getType() {
+        return "";
+    }
+
+    @Override
+    public void setBoundingBox(BoundingBox bbox) {
+
+    }
 }
